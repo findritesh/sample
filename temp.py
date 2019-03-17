3c385d53ac3cf998f15c61fa03315bf965f7fd4d
riteshsingh <coolritesh@gmail.com>
2019-03-17 00:30:43 -0400

    py commit

---
+++ temp.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class someClass(object):
+    def __init__(self, friends):
+        self.friends = friends
+    
+    def friendCircles(friends):
+        # Create a hashmap of person to friends
+        friendMap = {}
+        friendSet = set()
+        for i in range(len(friends)):
+            friendMap[i] = friends[i]
+            friendSet.add(i)
+        
+        circles = []
+        
+        while friendSet:
+            ele = friendSet.pop()
+            circleTemp = []
+            
+            tempSet = set()
+            tempSet.add(ele)
+            
+            while tempSet:
+                ls_friends = friendMap[ele]
+                
+                for j in range(len(ls_friends)):
+                    if j == ele:
+                        continue
+                    elif ls_friends[j] == "Y":
+                        circleTemp.append(j)
+                        tempSet.add(j)
+                if ele in tempSet:
+                    tempSet.remove(ele)
+            circles.append(circleTemp)
+            return len(circles)   
+    
+
+        
+            
+lst1 = [["Y","Y","N"],["Y","Y","N"],["N","N","Y"]]        
+
+a = someClass(lst1)
+a.findMedianSortedArrays(lst1)
