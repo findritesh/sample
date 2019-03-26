b0c15f372cd51a780bde1445ea313e063d46de03
Ritesh Singh <riteshsingh@Riteshs-MacBook-Pro.local>
2019-03-26 13:08:47 -0400

    something meaningful

---
+++ NewPythonFile.py
@@ -0,0 +1,9 @@
+# Aim is to run tensorflow
+# run some classification algorithm
+# run beautiful soup
+# call rest api's
+# plot 3d plot using flask
+# create blog
+
+msg = "hello world"
+print(msg)
--- temp1.py
+++ temp1.py
@@ -7,3 +7,9 @@ Created on Wed Mar 20 08:36:03 2019
 """
 
 #this is another commit
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
