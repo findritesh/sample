bc00ab3dd64631a3db20f6509b4707ce6ceb9ef8
Ritesh Singh <riteshsingh@Riteshs-MacBook-Pro.local>
2019-03-25 20:31:37 -0400

    commit from this

--- something.java
+++ something.java
@@ -1,5 +1,6 @@
 public class something {
     public static void main(String[]args){
         System.out.println("Hello, World");
+        Sytem.out.println("Hello From VSCode");
     }
 }
--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
+aa
 This is a temporary script file.
 """
 
